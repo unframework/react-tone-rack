4e7b601d7f25d975d1d06dfd6e8ea703332fdcf7
Nick Matantsev <nick.matantsev@gmail.com>
2021-10-06 01:05:38 -0400

    Add Tailwind CSS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 
+import 'tailwindcss/tailwind.css';
 import '../index.css';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
